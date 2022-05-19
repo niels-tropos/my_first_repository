6425559a9bcd39d2df13d8ac6de8c448def5a02c
Niels Palmans <101560764+niels-tropos@users.noreply.github.com>
2022-05-19 14:06:48 +0200

    Add files via upload

---
+++ python_vscode_test.py
@@ -0,0 +1,37 @@
+from random import random
+import math
+import matplotlib.pyplot as plt
+
+def fun(a):
+    steps = []
+    steps.append(a)
+
+    if (a % 2) == 0:
+        new = a / 2
+    else:
+        new = 3*a + 1
+
+    steps.append(new)
+
+    while new != 1:
+
+        if (new % 2) == 0:
+            new = new / 2
+        else:
+            new = 3*new + 1
+
+        steps.append(new)
+
+    return steps
+
+
+def number_generator(n, factor):
+    numbers = []
+    z = int(math.log(factor,10))
+
+    for i in range(0, n):
+        numbers.append(round(random(),z)*factor)
+
+    return numbers
+
+plt.plot([1,2,3],[1,4,9])
